1dbd5a436b283a6d0de9d423d9a2a7ec0ecc832e
UrielMaldonado14 <urielmaldonadooso@gmailcom>
2025-12-09 07:14:54 -0600

    Update 2

--- tests/Unit/CalculateWeightedGradeTest.php
+++ tests/Unit/CalculateWeightedGradeTest.php
@@ -16,13 +16,15 @@ class CalculateWeightedGradeTest extends TestCase
             ['grade' => 90, 'credits' => 5],
             ['grade' => 85, 'credits' => 4],
             ['grade' => 95, 'credits' => 3],
+            ['grade' => 95, 'credits' => 2],
         ];
 
         $result = $controller->calculateWeightedGrade($courses, 100);
 
         $this->assertEquals(89.58, $result['weighted_average']);
-        $this->assertEquals(13, $result['total_credits']);
+        // $this->assertEquals(12, $result['total_credits']);
         $this->assertEquals(3, $result['total_courses']);
+        $this->assertEquals(14, $result['total_credits']);
         $this->assertEquals('Muy bueno', $result['status']);
     }
 
